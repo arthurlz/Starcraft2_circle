d34edece255eae97d8dd504874241913808de500
loveppears <litar929@gmail.com>
2017-06-14 21:13:22 +0900

    :ballon:

--- app.js
+++ app.js
@@ -12,65 +12,13 @@ const logger = require('koa-logger')
 const app = new Koa();
 
 
-const User =require('./src/server/models/user');
+const user = require('./src/server/controller/user');
 app.use(require('koa-bodyparser')());
 app.use(json());
 app.use(logger());
 
-// app.use(async (next) => {
-//   let start = new Date;
-//   yield next;
-//   let ms = new Date - start;
-//   console.log('%s %s - %s', this.method, this.url, ms);
-// });
 
-// app.use(async (next) => {  //  如果JWT验证失败，返回验证失败信息
-//   try {
-//     yield next;
-//   } catch (err) {
-//     if (401 == err.status) {
-//       this.status = 401;
-//       this.body = {
-//         success: false,
-//         token: null,
-//         info: 'Protected resource, use Authorization header to get access'
-//       };
-//     } else {
-//       throw err;
-//     }
-//   }
-// });
-// app.use( async ( ctx ) => {
-//   ctx.body = "12312"
-// })
-
-router.get('/api/fuck', async (ctx) => {
-    ctx.body = 'hello guru'
-    var user = new User({
-      name: 'lita',
-      password: 'lita',
-      email: 'lita@gmail.com',
-      gender: 'M',
-      signature: 'cool',
-      personalWeb: 'litacheng.me',
-      GitHub: 'loveppears',
-      avatarUrl: 
-          'http://res.cloudinary.com/hezf/image/upload/v1467186691/vwuj8a3tpuqoy5fzuzlw.png'
-      ,
-      integration: '1'
-    });
-    user.save(function (err, res) {
-        if (err) {
-            console.log("Error:" + err);
-        }
-        else {
-            console.log("Res:" + res);
-        }
-
-    });
-})
-
-app.use(router.routes(), router.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
 
 app.on('error', function(err, ctx){
     
